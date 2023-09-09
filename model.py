a1e1fa7c65706e80ea20e57916043527d4119ae1
Tuxedomask95 <metue211@gmail.com>
2023-09-10 00:41:11 +0300

    mean squared error düzeltildi, rmse'ye çevirildi!

--- model.py
+++ model.py
@@ -625,10 +625,13 @@ accuracy = 100 - np.mean(mape)
 
 print('Accuracy:', round(accuracy, 2), '%.') # Accuracy: 86.98 %.
 
-from sklearn.metrics import mean_squared_error
-mse = mean_squared_error(y_test, y_pred)
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+mse = mean_squared_error(y_test, y_pred, squared=False)
 
 print("Mean Squared Error:", mse)
+
+mae = mean_absolute_error(y_test, y_pred)
+print('Mean Absoulte Error:',mae)
 
 
 mape = MAPE(y_test, y_pred)
